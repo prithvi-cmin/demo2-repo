d679b5887a9513cbc7e483c8542a29c0a2016811
prithvi-cmin <prithvi.nandy@cloudmagician.in>
2022-10-25 12:05:52 +0530

    3rd commit

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('<h1>HELLO WORLD</h1>')
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    .button {
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      margin: 4px 2px;
+      cursor: pointer;
+    }
+    
+    .button1 {background-color: #4CAF50;} /* Green */
+    .button2 {background-color: #008CBA;} /* Blue */
+    </style>
+    </head>
+    <body>
+    
+    <button class="button button1">Green</button>
+    <button class="button button2">Blue</button>
+    
+    </body>
+    </html>`)
 })
  const p = app.listen(process.env.PORT || 5000)
  console.log(p)
